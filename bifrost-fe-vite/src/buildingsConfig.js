ceaf86d71d8052b48589eb77729c1b9bbfff0f2d
JacquesJ44 <jacquesj44@gmail.com>
2026-05-15 15:34:17 +0200

    update pricing structure 150526

--- bifrost-fe-vite/src/buildingsConfig.js
+++ bifrost-fe-vite/src/buildingsConfig.js
@@ -1,10 +1,11 @@
 export const buildingPackages = {
   1: "set1",  // 4 on O
-  2: "set2",  // 6 on N
-  3: "set2",  // 126 on M
-  4: "set2",  // 82 on M
-  5: "set2",  // 343 on B
-  7: "set2",  // 100 on M
-  8: "set2",  // 146 on M
+  2: "set4",  // 6 on N
+  3: "set4",  // 126 on M
+  4: "set4",  // 82 on M
+  5: "set4",  // 343 on B
+  7: "set4",  // 100 on M
+  8: "set4",  // 146 on M
   6: "set3",  // TSY
+  10: "set4",  // 10 on V
 };
--- bifrost-fe-vite/src/packagesConfig.js
+++ bifrost-fe-vite/src/packagesConfig.js
@@ -1,7 +1,8 @@
 export const packageSets = {
-  set1: ["50/50Mbps - R750", "100/100Mbps - R950"],
+  set1: ["75/75Mbps - R799", "125/125Mbps - R999"],
   set2: ["50/50Mbps - R750", "100/100Mbps - R950", "200/200Mbps - R1 125"],
   set3: ["20/20Mbps - R599", "50/50Mbps - R899", "100/100Mbps - R1 099"],
+  set4: ["75/75Mbps - R799", "125/125Mbps - R999", "200/200Mbps - R1 125"],
 };
 
    
